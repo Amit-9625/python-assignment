3586fae369e7c99baf9c9e0e9acb61a1ce58a8d6
Amit-9625 <harryroy9625@gmail.com>
2026-01-05 12:28:19 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+#TASK1 
+num1=int(input("enter 1st number:"))
+num2=int(input("enter 2nd number:"))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
---
+++ task2.py
@@ -0,0 +1,5 @@
+#TASK2 
+first_name=str(input("enter your first name:"))
+last_name=str(input("enter your last name:"))
+print("hello,",first_name + " " + last_name ,"how are you doing?")
+ 
